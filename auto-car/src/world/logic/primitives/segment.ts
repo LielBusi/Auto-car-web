3af97b01b2efd0b019d08f084a83817caa0acb42
LielBusi <liel25778@gmail.com>
2025-08-09 12:30:52 +0300

    feat(segment.ts): Add segment class for graph management

---
+++ auto-car/src/world/logic/primitives/segment.ts
@@ -0,0 +1,87 @@
+import { Point } from "./point";
+import {
+  distance,
+  subtract,
+  normalize,
+  dot,
+  add,
+  scale,
+  magnitude,
+} from "../math/utils";
+
+export class Segment {
+  p1: Point;
+  p2: Point;
+  oneWay: boolean;
+
+  constructor(p1: Point, p2: Point, oneWay: boolean = false) {
+    this.p1 = p1;
+    this.p2 = p2;
+    this.oneWay = oneWay;
+  }
+
+  length(): number {
+    return distance(this.p1, this.p2);
+  }
+
+  directionVector(): Point {
+    return normalize(subtract(this.p2, this.p1));
+  }
+
+  equals(seg: Segment): boolean {
+    return this.includes(seg.p1) && this.includes(seg.p2);
+  }
+
+  includes(point: Point): boolean {
+    return this.p1.equals(point) || this.p2.equals(point);
+  }
+
+  distanceToPoint(point: Point): number {
+    const proj = this.projectPoint(point);
+    if (proj.offset > 0 && proj.offset < 1) {
+      return distance(point, proj.point);
+    }
+    const distToP1 = distance(point, this.p1);
+    const distToP2 = distance(point, this.p2);
+    return Math.min(distToP1, distToP2);
+  }
+
+  projectPoint(point: Point): { point: Point; offset: number } {
+    const a = subtract(point, this.p1);
+    const b = subtract(this.p2, this.p1);
+    const normB = normalize(b);
+    const scaler = dot(a, normB);
+    const projPoint = add(this.p1, scale(normB, scaler));
+    return {
+      point: projPoint,
+      offset: scaler / magnitude(b),
+    };
+  }
+
+  draw(
+    ctx: CanvasRenderingContext2D,
+    options: {
+      width?: number;
+      color?: string;
+      dash?: number[];
+      cap?: CanvasLineCap;
+    } = {}
+  ): void {
+    const { width = 2, color = "black", cap = "butt" } = options;
+    let { dash = [] } = options;
+
+    ctx.beginPath();
+    ctx.lineWidth = width;
+    ctx.strokeStyle = color;
+    ctx.lineCap = cap;
+
+    if (this.oneWay) {
+      dash = [4, 4];
+    }
+    ctx.setLineDash(dash);
+    ctx.moveTo(this.p1.x, this.p1.y);
+    ctx.lineTo(this.p2.x, this.p2.y);
+    ctx.stroke();
+    ctx.setLineDash([]);
+  }
+}
