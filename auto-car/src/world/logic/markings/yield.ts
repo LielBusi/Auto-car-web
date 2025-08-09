81cdd3dea5a608ab9fc85cbd723dfca1a459ccbd
LielBusi <liel25778@gmail.com>
2025-08-09 13:52:53 +0300

    feat(yield.ts): Add yield marking

---
+++ auto-car/src/world/logic/markings/yield.ts
@@ -0,0 +1,37 @@
+import { Marking } from "./marking";
+import { Point } from "../primitives/point";
+import { Segment } from "../primitives/segment";
+import { angle } from "../math/utils";
+
+export class Yield extends Marking {
+  border: Segment;
+
+  constructor(
+    center: Point,
+    directionVector: Point,
+    width: number,
+    height: number
+  ) {
+    super(center, directionVector, width, height);
+
+    this.border = this.poly.segments[2];
+    this.type = "yield";
+  }
+
+  draw(ctx: CanvasRenderingContext2D): void {
+    this.border.draw(ctx, { width: 5, color: "white" });
+    ctx.save();
+    ctx.translate(this.center.x, this.center.y);
+    ctx.rotate(angle(this.directionVector) - Math.PI / 2);
+    ctx.scale(1, 3);
+
+    ctx.beginPath();
+    ctx.textBaseline = "middle";
+    ctx.textAlign = "center";
+    ctx.fillStyle = "white";
+    ctx.font = `bold ${this.height * 0.3}px Arial`;
+    ctx.fillText("YIELD", 0, 1);
+
+    ctx.restore();
+  }
+}
