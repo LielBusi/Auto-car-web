ab5086be788ec1bb23a76f0e64ea006b59ec0a95
LielBusi <liel25778@gmail.com>
2025-08-09 13:47:31 +0300

    feat(start.ts): Add start marking

---
+++ auto-car/src/world/logic/markings/start.ts
@@ -0,0 +1,30 @@
+import { Marking } from "./marking";
+import { Point } from "../primitives//point";
+import { angle } from "../math/utils"; // Adjust path as needed
+
+export class Start extends Marking {
+  img: HTMLImageElement;
+
+  constructor(
+    center: Point,
+    directionVector: Point,
+    width: number,
+    height: number
+  ) {
+    super(center, directionVector, width, height);
+
+    this.img = new Image();
+    this.img.src = "car.png";
+    this.type = "start";
+  }
+
+  draw(ctx: CanvasRenderingContext2D): void {
+    ctx.save();
+    ctx.translate(this.center.x, this.center.y);
+    ctx.rotate(angle(this.directionVector) - Math.PI / 2);
+
+    ctx.drawImage(this.img, -this.img.width / 2, -this.img.height / 2);
+
+    ctx.restore();
+  }
+}
